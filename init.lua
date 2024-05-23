00c6e95555f6963594da5b5c3ded9066292f64bc
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2024-05-23 10:00:32 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = ' '
+
+-- For nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -6,7 +8,12 @@ require("jeddy3.lazy")
 
 vim.opt.wrap = false
 vim.opt.number = true
+
+-- Share clipboard with system
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Save
 vim.keymap.set({ 'n', 'i' }, '<C-s>', "<cmd>w<CR>", {})
@@ -19,6 +26,7 @@ vim.keymap.set('v', '<C-x>', 'd', {})
 -- Redo on U
 vim.keymap.set('n', 'U', "<C-r>", {})
 
+-- Ctrl-hjkl for switching windows (also in terminal)
 vim.keymap.set('n', '<C-j>', '<C-w>j', {})
 vim.keymap.set('n', '<C-k>', '<C-w>k', {})
 vim.keymap.set('n', '<C-l>', '<C-w>l', {})
@@ -28,3 +36,7 @@ vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j', {})
 vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k', {})
 vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l', {})
 vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', {})
+
+-- Buffer wipe on leader-w
+vim.keymap.set('n', '<leader>w', '<cmd>bw<CR>', {})
+
