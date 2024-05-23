17122528207804abe186ce69868a6325923fcf87
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2024-05-23 10:06:48 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -18,11 +18,6 @@ vim.opt.smartcase = true
 -- Save
 vim.keymap.set({ 'n', 'i' }, '<C-s>', "<cmd>w<CR>", {})
 
--- Cut, copy, paste
-vim.keymap.set('n', '<C-v>', 'Pl', {})
-vim.keymap.set('v', '<C-c>', 'y', {})
-vim.keymap.set('v', '<C-x>', 'd', {})
-
 -- Redo on U
 vim.keymap.set('n', 'U', "<C-r>", {})
 
