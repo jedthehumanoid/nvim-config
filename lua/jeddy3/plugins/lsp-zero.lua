7ecfe98f99244838c8e0a1612bfa5d5a586759ab
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2024-09-16 13:32:37 +0200

    Updates

--- lua/jeddy3/plugins/lsp-zero.lua
+++ lua/jeddy3/plugins/lsp-zero.lua
@@ -1,50 +1,36 @@
 return {
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		dependencies = {
-			{ 'neovim/nvim-lspconfig' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'L3MON4D3/LuaSnip' },
-		},
-		branch = 'v3.x',
-		config = function()
-			local lsp_zero = require('lsp-zero')
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v4.x',
+    config = function()
 
-			lsp_zero.on_attach(function(_, bufnr)
-				lsp_zero.default_keymaps({ buffer = bufnr, preserve_mappings = false })
-				lsp_zero.buffer_autoformat()
-			end)
-			local lspconfig = require('lspconfig')
-			lspconfig.clangd.setup({})
-			local lua_opts = lsp_zero.nvim_lua_ls()
-			lspconfig.lua_ls.setup(lua_opts)
-			lspconfig.rust_analyzer.setup({})
-			lspconfig.tsserver.setup({})
-			lspconfig.gopls.setup({})
-			lspconfig.svelte.setup({})
-			lspconfig.pylsp.setup({
-				settings = {
-					pylsp = {
-						plugins = {
-							flake8 = {
-								enabled = true
-							},
-							pycodestyle = {
-								enabled = false
-							},
-							mccabe = {
-								enabled = false
-							},
-							pyflakes = {
-								enabled = false
-							}
-						}
-					}
-				}
-			})
-			vim.diagnostic.config({ virtual_text = false })
-		end,
-	},
+    local lsp_zero = require('lsp-zero')
 
+    local lsp_attach = function(client, bufnr)
+        local opts = {buffer = bufnr}
+
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    end
+
+    lsp_zero.extend_lspconfig({
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    })
+    require('mason-lspconfig').setup({
+        handlers = {
+            function(server_name)
+                require('lspconfig')[server_name].setup({})
+            end,
+        }
+    })
+    end
 }
