8296d49ffaac6226771530b10aaa09a3a024be79
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2025-03-29 15:02:05 +0100

    Fixes

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,24 +1,8 @@
 return {
-<<<<<<< HEAD
-	'stevearc/oil.nvim',
+        'stevearc/oil.nvim',
 	lazy = false,
 	config = function()
 		require("oil").setup()
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end
-} 
-=======
-        'stevearc/oil.nvim',
-	lazy = false,
-	config = function()
-		require("oil").setup({
-			float = {
-				max_height = 0.8,
-				max_width = 0.8,
-				-- padding = 10,
-			},
-		})
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	end
 }
->>>>>>> 12e1a4c (Changes)
